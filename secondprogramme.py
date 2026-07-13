6b5808beb0b93c2767752a197c95e8a1121e31fc
kamma Hitesh <saihitesh1448@gmail.com>
2026-07-13 21:05:51 +0530

    Add files via upload

---
+++ secondprogramme.py
@@ -0,0 +1,93 @@
+class Book:
+    def __init__(self):
+        self.title="not entred"
+        self.author="not entred"
+        self.pages="not entred"
+        self.price=None
+        self.is_issued=False
+    def details(self):
+        self.title=input("enter the title of book: ")
+        self.author=input("enter the author name of book: ")
+        while True:
+            self.pages=int(input("enter the no of pages: "))
+            if self.pages>0:
+                break
+            else:
+                print("enter valid no of pages")
+        while True:
+            self.price=float(input("enter the price of book: "))
+            if self.price>0:
+                break
+            else:
+                print("enter a valid price")
+    def issue(self):
+        if self.is_issued:
+            print("book is alredy issued")
+        else:
+            self.is_issued=True
+            print("book is now issued")
+    def ret(self):
+        if self.is_issued:
+            self.is_issued=False
+            print("book is now returned")
+        else:
+            print("book is already returned")
+    def print_details(self):
+        print(f"Title: {self.title}")
+        print(f"Author: {self.author}")
+        print(f"Pages: {self.pages}")
+        print(f"Price: Rs {self.price}")
+        print(f"Status: {'Issued' if self.is_issued else 'Available'}")
+    def __str__(self):
+        return f"Book title:{self.title}, Book authour:{self.author}, Book price:{self.price}, Book pages:{self.pages}"
+    
+
+book=[]
+current=None
+while True:
+    print(".............menue...............\n")
+    print("1.enter details\n2.see details\n3.issue book\n4.return book\n5.see all books\n6.exit\n")
+    op=int(input("enter the option from above menue: "))
+    match (op):
+        case 1:
+            current=Book()
+            current.details()
+            book.append(current)
+            print("Details added successfully")
+        case 2:
+            if current is None:
+                print("please add a book first")
+            else:
+                current.print_details()
+                print()
+            
+        case 3:
+            if current is None:
+                print("please enter the book details")
+            else:
+                current.issue()
+        case 4:
+            if current is None:
+                print("please enter book details")
+            else:
+                current.ret()
+        case 5:
+            if not book:
+                print("please enter book details")
+            else:
+                print("---------ALL BOOKS----------")
+                for i in book:
+                    print(f"{i}\n")
+        case 6:
+            print("........YOU ARE EXITING..........")
+            break
+        case _:
+            print("please enter the valid option")
+            
+            
+        
+
+            
+
+        
+
